58b034eb06dbf2a0fde375f4e190c02a7cbbe358
David Doose <david.doose@gmail.com>
2024-10-28 09:23:16 +0100

    v 0.9

--- bin/main.rs
+++ bin/main.rs
@@ -1,46 +1,46 @@
 pub use sity::*;
 
 fn main() {
+    // generate_scale_mul().unwrap();
+    // generate_scale_div().unwrap();
+    // generate_scale_pow().unwrap();
+    // generate_scale_root().unwrap();
+
     let x = metre(123.0);
     let y: KiloMetre<_> = x.convert();
 
     println!("{}", x);
     println!("{}", y);
 
-    // generate_scale_mul().unwrap();
-    // generate_scale_div().unwrap();
-    // generate_scale_pow().unwrap();
-    // generate_scale_root().unwrap();
+    let x = metre(3.0);
+    let y: CentiMetre<_> = x.convert();
 
-    // let x = SI::m(3.0);
-    // let y: CentiMetre<_> = SI::convert(x);
+    println!("{} = {}", x, y);
+    let m = metre(1.0);
+    let g = kilo_gram(2.0);
+    let s = second(3.0);
+    let a = ampere(4.0);
 
-    // println!("{} = {}", x, y);
-    // let m = metre(1.0);
-    // let g = kilo_gram(2.0);
-    // let s = second(3.0);
-    // let a = ampere(4.0);
+    let x = m.pow2() * g / s.pow3() / a;
+    println!("{}", x);
+    let y = volt(1.2);
+    println!("{}", y);
 
-    // let x = m.pow2() * g / s.pow3() / a;
-    // println!("{}", x);
-    // let y = volt(1.2);
-    // println!("{}", y);
+    println!("{}", hertz(2.0));
+    println!("{}", watt(3.0));
 
-    // println!("{}", hertz(2.0));
-    // println!("{}", watt(3.0));
+    let b = centi_metre(4.0);
+    let c = second(2.0);
+    println!("a = {}", a);
+    println!("b = {}", b);
+    println!("c = {}", c.pow2().root2());
+    let si_value = a / c;
+    use std::mem;
+    let si_value_size = mem::size_of_val(&si_value);
+    // let x = a + c; // Compile time Error
 
-    // let b = centi_metre(4.0);
-    // let c = second(2.0);
-    // println!("a = {}", a);
-    // println!("b = {}", b);
-    // println!("c = {}", c.pow2().root2());
-    // let si_value = a / c;
-    // use std::mem;
-    // let si_value_size = mem::size_of_val(&si_value);
-    // // let x = a + c; // Compile time Error
-
-    // println!("si_value = {} (size = {})", si_value, si_value_size);
-    // let pure_value = si_value.value();
-    // let pure_value_size = mem::size_of_val(&pure_value);
-    // println!("pure_value = {} (size = {})", pure_value, pure_value_size);
+    println!("si_value = {} (size = {})", si_value, si_value_size);
+    let pure_value = si_value.value();
+    let pure_value_size = mem::size_of_val(&pure_value);
+    println!("pure_value = {} (size = {})", pure_value, pure_value_size);
 }
